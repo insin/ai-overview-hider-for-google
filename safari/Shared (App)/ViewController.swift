77f485eec24c86dce414d8e19d83f7ca278a3b0e
Jonny Buchanan <jonathan.buchanan@gmail.com>
2024-12-27 21:47:44 +1000

    Xcode project: - resolve CURRENT_PROJECT_VERSION and MARKETING_VERSION from build settings,\\ - fix build warnings about missing Assets - fix bundle identifier

--- safari/Shared (App)/ViewController.swift
+++ safari/Shared (App)/ViewController.swift
@@ -9,7 +9,7 @@ import SafariServices
 typealias PlatformViewController = NSViewController
 #endif
 
-let extensionBundleIdentifier = "soitis.dev.AI-Overview-Hider-for-Google.Extension"
+let extensionBundleIdentifier = "dev.soitis.AI-Overview-Hider-for-Google.Extension"
 
 class ViewController: PlatformViewController, WKNavigationDelegate, WKScriptMessageHandler {
 
